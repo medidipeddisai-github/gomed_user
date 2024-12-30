ef38c8ff2d31c98934ea05e9e1a3e8ee9f3b4b16
medidipeddisai <saimedidi999@gmail.com>
2024-12-30 12:23:21 +0530

    2nd time updated

--- lib/ordertracking.dart
+++ lib/ordertracking.dart
@@ -56,7 +56,7 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Order is $orderStatus',
+              'Order i $orderStatus',
               style: TextStyle(
                 fontSize: screenWidth * 0.06,
                 fontWeight: FontWeight.bold,
